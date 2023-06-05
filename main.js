d31cd2ba106746ee00e2dce1b4f95e7f64e70b95
A6Coding <111938126+A6Coding@users.noreply.github.com>
2023-06-05 16:34:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+
+function setup() {
+    video = createCapture(VIDEO); //Creates camera
+    video.size(550, 500); // sets size by width,height
+    canvas = createCanvas(550, 500); // creates canvas
+}
+
+function draw() {
+
+}
